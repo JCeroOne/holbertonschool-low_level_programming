632e44d98a07d1189bd881f6d22ecc37713e7370
JCeroOne <jceroone@gmail.com>
2025-10-23 22:59:15 +0000

    Update

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints the numbers from 0 to 14
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 15; i++)
+	{
+		if (i <= 10)
+		{
+			/*_putchar ('0' + i);*/
+		}
+		_putchar ('0' + i);
+	}
+	_putchar ('\n');
+}
