1ed82eafb371d1620358c9761d9c45568f9bfacd
JCeroOne <jceroone@gmail.com>
2025-10-23 22:30:25 +0000

    Update

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - Prints the numbers from 0 to 9.
+ */
+void print_numbers(){
+	for(int i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+}
