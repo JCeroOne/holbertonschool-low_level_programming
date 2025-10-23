ecc714933a142efbb64c278d659ce01cb2d9e7c9
JCeroOne <jceroone@gmail.com>
2025-10-23 23:51:00 +0000

    Update

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * main - Runs the program
@@ -14,33 +14,18 @@ int main(void)
 		{
 			if (i % 3 == 0)
 			{
-				_putchar('F');
-				_putchar('i');
-				_putchar('z');
-				_putchar('z');
+				printf("Fizz");
 			}
 			if (i % 5 == 0)
 			{
-				_putchar('B');
-				_putchar('u');
-				_putchar('z');
-				_putchar('z');
+				printf("Buzz");
 			}
 		}
 		else 
 		{
-			int h, t, u;
-
-			h = i % 100;
-			_putchar('0' + h);
-
-			t = (i % 100) % 10;
-			_putchar('0' + t);
-
-			u = (i % 100) / 10;
-			_putchar('0' + u);
+			printf(i);
 		}
-		_putchar(' ');
+		printf(" ");
 	}
 
 	return (0);
