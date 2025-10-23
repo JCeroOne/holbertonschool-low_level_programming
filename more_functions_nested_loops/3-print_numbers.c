4f306bddae2278ee162c3c88a8770442c56a23f5
JCeroOne <jceroone@gmail.com>
2025-10-23 22:32:47 +0000

    Update

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -5,9 +5,10 @@
  * print_numbers - Prints the numbers from 0 to 9.
  */
 void print_numbers(){
-	for(int i = 0; i < 10; i++)
+	int i;
+	for(i = 0; i < 10; i++)
 	{
 		putchar(i);
 	}
-	putchar("\n");
+	putchar('\n');
 }
