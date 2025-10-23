02c1ed8ebf6427bda01230a1497ed4bb0fda02ef
JCeroOne <jceroone@gmail.com>
2025-10-23 23:02:48 +0000

    Update

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -11,9 +11,9 @@ void more_numbers(void)
 	{
 		if (i <= 10)
 		{
-			/*_putchar ('0' + i);*/
+			_putchar ('0' + (i / 10));
 		}
-		_putchar ('0' + i);
+		_putchar ('0' + (i % 10));
 	}
 	_putchar ('\n');
 }
