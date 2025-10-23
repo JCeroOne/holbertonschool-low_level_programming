b1be8acb7cf6eaab64a9ce14a63c2e160187c5bc
JCeroOne <jceroone@gmail.com>
2025-10-23 23:46:02 +0000

    Update

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * main - Runs the program
+ * Return: 0 if succesful
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			if (i % 3 == 0)
+			{
+				_putchar('F');
+				_putchar('i');
+				_putchar('z');
+				_putchar('z');
+			}
+			if (i % 5 == 0)
+			{
+				_putchar('B');
+				_putchar('u');
+				_putchar('z');
+				_putchar('z');
+			}
+		}
+		else 
+		{
+			int h, t, u;
+
+			h = i % 100;
+			_putchar('0' + h);
+
+			t = (i % 100) % 10;
+			_putchar('0' + t);
+
+			u = (i % 100) / 10;
+			_putchar('0' + u);
+		}
+		_putchar(' ');
+	}
+}
