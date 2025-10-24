7d8a1debd57086a8cf66cbd2be3634f4fc6b5007
JCeroOne <jceroone@gmail.com>
2025-10-24 01:11:17 +0000

    Update

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -8,7 +8,7 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	if(size <= 0)
+	if (size <= 0)
 	{
 		_putchar('\n');
 	}
@@ -17,9 +17,9 @@ void print_triangle(int size)
 	{
 		for (j = 0; j < size; j++)
 		{
-			if (j < size - 1 - i) 
+			if (j < size - 1 - i)
 				_putchar(' ');
-			else 
+			else
 				_putchar('#');
 		}
 		_putchar('\n');
