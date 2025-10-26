eec98298bcdcc701cce5f7e2348993984a8ad709
JCeroOne <jceroone@gmail.com>
2025-10-26 20:53:56 +0000

    Update

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of the passed string, plus a newline.
+ * @str: The string to print.
+ */
+void puts2(char *str)
+{
+	int l;
+
+	for (l = 0; str[l]; l++)
+	{
+		if (l % 2 == 1)
+			_putchar(str[l]);
+	}
+
+	_putchar('\n');
+}
