b0a1eadf1622ac3bad9e2c02637112b2a57c1f7e
JCeroOne <jceroone@gmail.com>
2025-10-26 20:11:38 +0000

    Update

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses the passed string.
+ * @s: The string to reverse.
+ */
+void rev_string(char *s)
+{
+	int l = 0;
+	int i;
+	char s2 = s;
+
+	while (s[l])
+		l++;
+
+	for (i = 0; i < l; i++)
+	{
+		s[i] = s2[l - 1 - i];
+	}
+}
