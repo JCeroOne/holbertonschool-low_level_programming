d72c32579c34adb6c9e1ba36e17b3e0d6de56d5f
JCeroOne <jceroone@gmail.com>
2025-10-26 21:38:31 +0000

    Update

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - Prints the first n elements of the passed array.
+ * @a: The array to print.
+ * @b: The number of elements to print.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf(a[i]);
+		printf(", ");
+	}
+	printf("\n");
+}
