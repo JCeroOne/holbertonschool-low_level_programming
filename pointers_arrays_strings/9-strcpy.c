31dcc670156b31033622a31e28fc8e81f10824bd
JCeroOne <jceroone@gmail.com>
2025-10-26 21:47:04 +0000

    Update

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copies a string to another address.
+ * @dest: The destination address
+ * @src: The string to copy
+ *
+ * Return: The pointer to the destination.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	*dest = src;
+	return (*dest);
+}
