fb5db12aefd2b6edeffa7f3627268be980118312
JCeroOne <jceroone@gmail.com>
2025-10-27 22:19:17 +0000

    Update

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * reverse_array - Reverses up to n elements of the passed array.
+ * @a: The array to reverse.
+ * @n: The number of characters to swap.
+ */
+void reverse_array(int *a, int n)
+{
+	int l = 0;
+	int i;
+
+	while (a[l])
+		l++;
+
+	if (n < l)
+		n = l;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		int tmp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = tmp;
+	}
+}
