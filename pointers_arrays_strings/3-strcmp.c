e3b3657340a441101e32288459f428cfdfb2f800
JCeroOne <jceroone@gmail.com>
2025-10-27 22:07:01 +0000

    Update

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings, and returns which one is greater.
+ * @s1: The first string.
+ * @s2: The second string.
+ *
+ * Return: The difference between the strings.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] == s2[i])
+			continue;
+
+		return (s2[i] - s1[i]);
+	}
+
+	return 0;
+}
