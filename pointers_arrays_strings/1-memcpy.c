6678c67785ddc98ed8e6701cda0471a309e91223
JCeroOne <jceroone@gmail.com>
2025-10-30 02:19:48 +0000

    Update

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - Copies n bytes of memory from src to dest
+ * @dest: The address to copy the bytes to
+ * @src: The address to copy the bytes from
+ * @n: The number of bytes to copy
+ *
+ * Return: A pointer to the dest address
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
