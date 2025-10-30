3a3a38796e9a793ac3526a15e8428883e85cba7f
JCeroOne <jceroone@gmail.com>
2025-10-30 02:07:28 +0000

    Update

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - Fills in n bytes in memory after s with byte b
+ * @s: The starting point of the filling
+ * @n: The number of bytes to fill
+ * @b: The byte to fill with
+ *
+ * Return: A pointer to the memory area
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	
+	return (s);
+}
