e605067e40748a280603636dc6c9ebbbbeff5eb1
root <root@ip-10-42-186-223.ec2.internal>
2025-11-02 01:27:13 +0000

    Update

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Searches s for the first match of any character in accept.
+ * @s: The string to search through.
+ * @accept: The characters to search for.
+ *
+ * Return: A pointer to the first match found.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int l = 0;
+	int i;
+
+	while (accept[l])
+		l++;
+
+	while (1)
+        {
+                for (i = 0; i < l; i++)
+		{
+			if (accept[i] == *s)
+				return (s);
+		}
+
+                else if (*s == '\0')
+                        return (NULL);
+                s++;
+        }	
+}
