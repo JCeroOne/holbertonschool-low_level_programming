2a7091a611461dc2de0e7c3b96e00bff3031ed26
root <root@ip-10-42-186-223.ec2.internal>
2025-11-02 01:28:47 +0000

    Update

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -23,7 +23,7 @@ char *_strpbrk(char *s, char *accept)
 				return (s);
 		}
 
-                else if (*s == '\0')
+                if (*s == '\0')
                         return (NULL);
                 s++;
         }	
