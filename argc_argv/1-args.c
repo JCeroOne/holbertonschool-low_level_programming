c843a97225e709f5cd9cdc3cea6f9038a35185b3
JCeroOne <jceroone@gmail.com>
2025-11-02 23:26:40 +0000

    Update

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program.
+ * @argc: The argument count.
+ * @argv: The arguments.
+ *
+ * Return: Always 0
+ */
+int main(
+		int argc __attribute__((unused)),
+		char *argv[] __attribute__((unused))
+	)
+{
+	printf("%d\n", argc);
+	return (0);
+}
