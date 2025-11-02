b7e72ac0667d77c9b383f561373d1823f66d1637
JCeroOne <jceroone@gmail.com>
2025-11-02 22:32:58 +0000

    Update

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+int recursive_search(int base, int target)
+{
+	if (base * base > target)
+		return (-1);
+	if (base * base == target)
+		return (base);
+	return (recursive_search(base + 1, target));
+}
+
 /**
  * _sqrt_recursion - Returns the natural square root of the passed number.
  * @n: The number.
@@ -13,13 +22,4 @@ int _sqrt_recursion(int n)
 	if (n == 0)
 		return (0);
 	return recursive_search(1, n);
-}
-
-int recursive_search(int base, int target)
-{
-	if (base * base > target)
-		return (-1);
-	if (base * base == target)
-		return (base);
-	return (recursive_search(base + 1, target));
 }
