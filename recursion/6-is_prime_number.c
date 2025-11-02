5bc70e59c5553ad39cb32b015f98551034232fb2
JCeroOne <jceroone@gmail.com>
2025-11-02 22:44:56 +0000

    Update

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * recursive_search - Checks if target is a prime number recursively.
+ * @base: The number to start the search from.
+ * @target: The number to check.
+ *
+ * Return: 1 if target is prime, 0 otherwise.
+ */
+int recursive_search(int base, int target)
+{
+	if (target % base == 0)
+		return (0);
+	if (target == base)
+		return (1);
+	return (recursive_search(base + 2, target));
+}
+/**
+ * is_prime_number - Checks if n is prime.
+ * @n: The number to check.
+ *
+ * Return: 1 if n is prime, 0 otherwise.
+ */
+
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	if (n == 2)
+		return (1);
+	if (n % 2 == 0)
+		return (0);
+	return (recursive_search(3, n));
+}
