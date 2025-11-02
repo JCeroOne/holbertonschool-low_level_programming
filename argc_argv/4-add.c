e98413f31d230b34c92bb0994e0de7fb2bf7ffe3
JCeroOne <jceroone@gmail.com>
2025-11-02 23:43:35 +0000

    Update

---
+++ argc_argv/4-add.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * isInt - Check if a string is an integer.
+ * @str: The string to check.
+ *
+ * Return: 1 if str is an integer, 0 otherwise.
+ */
+int isInt(char *str)
+{
+	while (*str != '\0')
+	{
+		if (!isdigit(*str))
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ * main - Multiplies two numbers.
+ * @argc: The argument count.
+ * @argv: The arguments.
+ *
+ * Return: Always 0, unless something is off.
+ */
+int main(
+		int argc __attribute__((unused)),
+		char *argv[] __attribute__((unused))
+	)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		if (!isInt(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+			sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
