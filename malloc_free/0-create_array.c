6da03d8579ad69f7bb9a1b339336b88928546155
JCeroOne <jceroone@gmail.com>
2025-11-06 23:20:08 +0000

    Update

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -13,6 +13,9 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *arr = malloc(size * sizeof(char));
+	
+	if (arr == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 	{
