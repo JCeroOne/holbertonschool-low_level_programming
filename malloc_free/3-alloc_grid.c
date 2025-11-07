b156014027177372eb3358e698e5c7b748f610ff
JCeroOne <jceroone@gmail.com>
2025-11-07 00:02:19 +0000

    Update

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Allocates a grid in memory.
+ * @width: The width of the grid.
+ * @height: The height of the grid.
+ *
+ * Return: A pointer to the grid, or NULL if something fails.
+ */
+int **alloc_grid(int width, int height)
+{
+	unsigned int **grid;
+	unsigned int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(height * sizeof(*int));
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+
+		if (grid[i] == NULL)
+			return (NULL);
+
+		for (j = 0; j < width; j++)
+			grid[i][j] = '0';
+	}
+
+	return (grid);
+}
