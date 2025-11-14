a83953d8f8050084b818937b351f8dd6c0f7967c
JCeroOne <jceroone@gmail.com>
2025-11-14 01:58:42 +0000

    Update

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,6 +1,7 @@
 #include "function_pointers.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 /**
  * array_iterator - Iterates over an array, and executes the action function on each element.
