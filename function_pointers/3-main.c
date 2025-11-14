ef6059fb878a3ddd7e1e6ce804cceaf04ea1be6d
JCeroOne <jceroone@gmail.com>
2025-11-14 03:02:46 +0000

    Update

---
+++ function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Executes the program.
+ * @argc: The argument count.
+ * @argv: The arguments.
+ *
+ * Return: Always 0, unless something fails.
+ */
+int main(
+	int argc __attribute__((unused)),
+	char *argv[] __attribute__((unused))	
+)
+{
+	if(argc != 4)
+	{
+		printf("Error");
+		exit(98);
+	}
+
+	if(get_op_func(argv[2]) == NULL)
+	{
+		printf("Error");
+		exit(99);
+	}
+
+	if((argv[2] == '%' && argv[2] == '/') && argv[3] == '0')
+	{
+		printf("Error");
+		exit(100);
+	}
+
+	printf("%d", (get_op_func(argv[2]))(argv[1] - '0', argv[3] - '0'));
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ * op_add - Adds two numbers.
+ * @a: Number
+ * @b: Number
+ *
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Substracts a number from another.
+ * @a: Number
+ * @b: Number
+ *
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplies two numbers.
+ * @a: Number
+ * @b: Number
+ *
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divides two numbers.
+ * @a: Number
+ * @b: Number
+ *
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - a % b
+ * @a: Number
+ * @b: Number
+ *
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
