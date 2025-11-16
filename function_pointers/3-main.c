22b7117f9234ab24ca39ae3b84b766c862cc5c1e
JCeroOne <jceroone@gmail.com>
2025-11-16 20:19:56 +0000

    Update

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -11,28 +11,28 @@
  */
 int main(
 	int argc __attribute__((unused)),
-	char *argv[] __attribute__((unused))	
+	char *argv[] __attribute__((unused))
 )
 {
-	if(argc != 4)
+	if (argc != 4)
 	{
 		printf("Error\n");
 		return (98);
 	}
 
-	if(get_op_func(argv[2]) == NULL)
+	if (get_op_func(argv[2]) == NULL)
 	{
 		printf("Error\n");
 		return (99);
 	}
 
-	if((*argv[2] == '%' || *argv[2] == '/') && atoi(argv[3]) == 0)
+	if ((*argv[2] == '%' || *argv[2] == '/') && atoi(argv[3]) == 0)
 	{
 		printf("Error\n");
 		return (100);
 	}
-	
+
 	printf("%d\n", (get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
-	
+
 	return (0);
 }
