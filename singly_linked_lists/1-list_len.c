3c06f1f2ca49d1c78c827698e8eaad512387d144
JCeroOne <jceroone@gmail.com>
2025-11-29 19:50:28 +0000

    Update

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - Determines the length of a linked list
+ * @h: The first node of the list
+ *
+ * Returns: The length of the list
+ */
+size_t list_len(const list_t *h)
+{
+	const list_t *node = h;
+	size_t len = 0;
+
+	while(node != NULL)
+	{
+		node = node->next;
+		len++;
+	}
+
+	return (len);
+}
