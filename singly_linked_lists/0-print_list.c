1e5687ca4f5f90787fc04d60829450acde3cc0a2
JCeroOne <jceroone@gmail.com>
2025-11-29 18:25:01 +0000

    Update

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,13 +8,13 @@ size_t print_list(const list_t *h)
 	size_t len = 0;
 	list_t *node = h;
 
-	while(*node != NULL)
+	while(node != NULL)
 	{
 		if(*node->str == NULL)
 			printf("[0] (nil)");
 		else
-			printf("[%d] %s", strlen(*node->str), *node->str);
-		node = *node->h;
+			printf("[%zu] %s", strlen(*node->str), *node->str);
+		node = *node->next;
 		len++;
 	}
 
