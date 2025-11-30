d633b4939fadd1ced7fe6780a077f7c9069d1df3
JCeroOne <jceroone@gmail.com>
2025-11-30 05:42:46 +0000

    Patch

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -33,7 +33,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	node->next = current;
 
 	if (current == NULL)
+	{
 		node->prev = NULL;
+		*h = node;
+	}
 	else
 	{
 		node->prev = current->prev;
