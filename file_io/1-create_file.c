0bd088caa20253d893719bfffaa8905ceacae97f
JCeroOne <jceroone@gmail.com>
2025-12-01 02:48:10 +0000

    Patch

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -28,6 +28,12 @@ int create_file(const char *filename, char *text_content)
 		if(file == -1)
 			return (-1);
 	}
+	
+	if (text_content == NULL)
+	{
+		close(file);
+		return (1);
+	}
 
 	while (text_content[content_length])
 		content_length++;
