6c5c666534cdafc0e197c253b42fbb6498d69293
JCeroOne <jceroone@gmail.com>
2025-12-01 02:45:46 +0000

    Patch

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_RDWR);
+	file = open(filename, O_RDWR | O_TRUNC);
 
 	if (file == -1)
 	{
