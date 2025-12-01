3ad375c6991567364c67aaf6cc3ea0dc28094ad5
JCeroOne <jceroone@gmail.com>
2025-12-01 04:15:14 +0000

    New task

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -13,23 +13,23 @@
  */
 int copyBySegments(int from, int to, size_t n)
 {
-        ssize_t bytesRead;
-        ssize_t bytesWritten = 0;
-        char *buffer = malloc(n * sizeof(char));
+	ssize_t bytesRead;
+	ssize_t bytesWritten = 0;
+	char *buffer = malloc(n * sizeof(char));
 
-        while ((bytesRead = read(from, buffer, n)) > 0)
+	while ((bytesRead = read(from, buffer, n)) > 0)
 	{
-                bytesWritten = write(to, buffer, bytesRead);
+		bytesWritten = write(to, buffer, bytesRead);
 
-                if(bytesWritten != bytesRead)
-                {
-                        free(buffer);
-                        return (-1);
-                }
-        }
+		if(bytesWritten != bytesRead)
+		{
+			free(buffer);
+			return (-1);
+		}
+	}
 
-        free(buffer);
-        
+	free(buffer);
+
 	if (bytesRead == -1)
 		return (-1);
 
@@ -53,38 +53,31 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	file_from = open(argv[1], O_RDONLY);
-
 	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
 	if (file_to == -1)
 	{
 		close(file_from);
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-
-	if(copyBySegments(file_from, file_to, 1024) == -1)
+	if (copyBySegments(file_from, file_to, 1024) == -1)
 	{
 		close(file_from);
 		close(file_to);
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 	}
-
 	if (close(file_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
-
-	if(close(file_to) == -1)
+	if (close(file_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
 		exit(100);
