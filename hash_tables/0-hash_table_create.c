833f503d670777b7964cc42e138ff20070cbcf38
JCeroOne <jceroone@gmail.com>
2025-12-14 23:37:30 +0000

    Patch

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -13,26 +13,20 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ptr = malloc(sizeof(hash_table_t));
 	hash_node_t *arr = malloc(sizeof(hash_node_t) * size);
-	hash_node_t **a2 = NULL;
 
-	*a2 = arr;
-
-	if (ptr == NULL)
+	if (ptr == NULL || arr == NULL)
 	{
+		if (ptr != NULL)
+			free(ptr);
+		
 		if (arr != NULL)
 			free(arr);
 
 		return (NULL);
 	}
 
-	if (arr == NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-
 	ptr->size = size;
-	ptr->array = a2;
+	ptr->array = &arr;
 
 	return (ptr);
 }
