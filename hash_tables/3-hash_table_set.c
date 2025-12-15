6a3c71e97585267d28a14ea40aa91ed33ebd81e2
JCeroOne <jceroone@gmail.com>
2025-12-15 03:28:50 +0000

    Patch

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -25,7 +25,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	start = ht->array[index];
 	current = start;
 
-	while (current)
+	while (current != NULL)
 	{
 		if (strcmp(strdup(key), current->key) == 0){
 			current->value = strdup(value);
