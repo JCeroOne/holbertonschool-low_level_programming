3f72bfd5d561780d7b8c9842162f67fd17df3cea
JCeroOne <jceroone@gmail.com>
2025-12-15 04:45:30 +0000

    Patch

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *element = malloc(sizeof(hash_node_t));
+	hash_node_t *element;
 	hash_node_t *start;
 	hash_node_t *current;
 
@@ -33,17 +33,24 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			current->value = strdup(value);
 			return (1);
 		}
-
 		current = current->next;
 	}
+	element = malloc(sizeof(hash_node_t));
+	if(element == NULL)
+		return (0);
 
-	free(element->value);
-
-	element->key = key;
+	element->key = strdup(key);
 	element->value = strdup(value);
+	if (element->key == NULL || element->value == NULL)
+	{
+		if (element->key != NULL)
+			free(element->key);
+		if (element->value != NULL)
+			free(element->value);
+		free(element);
+		return (0);
+	}
 	element->next = start;
-
 	ht->array[index] = element;
-
 	return (1);
 }
