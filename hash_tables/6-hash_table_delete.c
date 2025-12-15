41d7a8060f5adc3811d1aa31e20c929f3706feb9
JCeroOne <jceroone@gmail.com>
2025-12-15 04:21:35 +0000

    Patch

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -7,7 +7,7 @@
  * hash_table_delete - Deletes a hash table and its elements
  * @ht: The hash table to delete
  */
-void hash_table_print(const hash_table_t *ht)
+void hash_table_print(hash_table_t *ht)
 {
 	unsigned long int i;
 
