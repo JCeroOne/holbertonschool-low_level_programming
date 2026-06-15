b23a47fcee95e5e8f472f02019eaefb734c3dd8d
JCeroOne <jceroone@gmail.com>
2026-06-15 17:50:40 +0000

    Update

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Prints the numbers from 0 to 10.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
